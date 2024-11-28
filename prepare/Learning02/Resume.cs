03e1bbcdde13dff7314dd9bd70cfe58b93ef4673
kingsleydesigns <ezepuekingsley@gmail.com>
2024-11-28 20:15:51 +0100

    Week 2 Abstraction learning activity

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,13 @@
+public class Resume{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    public void DisplayJobs()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job i in _jobs)
+        {
+            i.Display();
+        }
+    }
+}
