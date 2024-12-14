81c2f9942491785de58043f4cc70100c52ddbd22
kingsleydesigns <ezepuekingsley@gmail.com>
2024-12-14 10:41:41 +0100

    Week 5 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+       var Assign1 = new Assignment ("Samuel Bennett", "Multiplication");
+       Console.WriteLine(Assign1.GetSummary());
+       Console.WriteLine();
+
+
+       var Math1 = new MathAssignment ("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+       Console.WriteLine(Math1.GetSummary());
+       Console.WriteLine(Math1.GetHomeworkList());
+       Console.WriteLine();
+
+
+       var Write1 = new WritingAssignment ("Mary Waters", "European History", "The Causes of World War II");
+       Console.WriteLine(Write1.GetSummary());
+       Console.WriteLine(Write1.GetWritingInformation());
+       Console.WriteLine();
+
+       
     }
 }
