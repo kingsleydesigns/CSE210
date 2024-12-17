1dc0ebb39ecaaef53400b798a20723fad94658cc
kingsleydesigns <ezepuekingsley@gmail.com>
2024-12-18 00:01:55 +0100

    Week 6 Learning Activvity (Shape)

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        var square = new Square ("red", 4.00);
+        var rect = new Rectangle ("blue", 4.00, 2.00);
+        var circle = new Circle ("green", 7.00);
+        
+        List<Shape> shape = new List<Shape>();
+        shape.Add (square);
+        shape.Add (rect);
+        shape.Add (circle);
+
+        foreach (Shape s in shape)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+            Console.WriteLine();
+        }
+        {
+            
+        }
+
     }
 }
